29cfb0c30c2aadb26e34d3fe6d3b22517bee6f43
Nathan Shaw <n.shaw@entando.com>
2023-07-19 13:40:27 -0400

    Update subscriber-mfe to listen for customevent

--- microfrontends/subscriber-mfe/src/App.js
+++ microfrontends/subscriber-mfe/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({ name }) {
+  return name ? (<h2>Just got a greeting from {name}</h2>)
+    : (<h2>Waiting for a greeting...</h2>);
 }
 
 export default App;
--- microfrontends/subscriber-mfe/src/custom-elements/WidgetElement.js
+++ microfrontends/subscriber-mfe/src/custom-elements/WidgetElement.js
@@ -1,15 +1,39 @@
-import './public-path.js';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from '../App';
 
+const EVENTS = {
+  greeting: 'greeting',
+};
+
 class WidgetElement extends HTMLElement {
-    connectedCallback() {
-        this.mountPoint = document.createElement('div');
-        this.appendChild(this.mountPoint);
-        const root = ReactDOM.createRoot(this.mountPoint);
-        root.render(<App />);
-   }
+
+  constructor() {
+    super();
+    this.name = null;
+    this.subscribeToWidgetEvent(EVENTS.greeting, (evt) => this.onGreeting(evt.detail.name));
+  }
+
+  connectedCallback() {
+    this.mountPoint = document.createElement('div');
+    this.appendChild(this.mountPoint);
+    this.render();
+  }
+
+  subscribeToWidgetEvent(eventType, eventHandler) {
+    window.addEventListener(eventType, eventHandler);
+  }
+
+  onGreeting(name) {
+    this.name = name;
+    this.render();
+  }
+
+  render() {
+    ReactDOM.render(<App name={this.name} />, this.mountPoint);
+  }
 }
 
 customElements.define('subscriber-mfe', WidgetElement);
+
+export default WidgetElement;
